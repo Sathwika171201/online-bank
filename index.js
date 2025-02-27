ac79b32bac8694f7b56a52f6ac433885ce80cf76
Sathwika171201 <molugurisathwika@gmail.com>
2025-02-27 11:00:57 +0530

    cc

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 
 let users = [];
-let user = {}
-// let useremail = "";
-// let username = "";
-// let currBalance = 0;
+let user = {};
 document.write("<div id=root></div>");
+function showHeader() {
+  let str = `
+  <div class="row bg-secondary">
+          <div class="col-lg-8">
+            <h1>My Bank</h1>
+          </div>
+          <div class="col-lg-4 text-end">
+          <button onclick='showHome()'>Home</button>
+          ${
+            user.email
+              ? "<button onclick='showLogout()'>Logout</button>"
+              : "<button onclick='showLogin()'>Login</button>"
+          }
+            <!-- <button onclick="toggle('dark')" class="btn btn-dark">Dark</button>
+            <button onclick="toggle('light')" class="btn btn-light">Light</button> -->
+          </div>
+        </div>
+  `;
+  header.innerHTML = str;
+}
+
+function toggle(color) {
+  if (color == "dark") {
+    document.body.style.background = "black";
+    document.body.style.color = "white";
+  } else {
+    document.body.style.background = "white";
+    document.body.style.color = "black";
+  }
+}
 function showUser() {
   if (document.getElementById("type").value == "3") {
-    console.log("Transfer")
-    selUser.style.display = 'block'
+    console.log("Transfer");
+    selUser.style.display = "block";
     let str = "<option value=0>--Select--</option>";
     for (let i = 0; i < users.length; i++) {
       if (users[i].email != user.email) {
@@ -16,9 +43,8 @@ function showUser() {
       }
     }
     selUser.innerHTML = str;
-  }
-  else {
-    selUser.style.display = "none"
+  } else {
+    selUser.style.display = "none";
   }
 }
 function saveData() {
@@ -27,7 +53,7 @@ function saveData() {
   for (let i = 0; i < users.length; i++) {
     if (users[i].email == user.email) {
       if (type == "1") {
-        console.log("testing")
+        console.log("testing");
         users[i].balance += amount;
         spBalance.innerHTML = users[i].balance;
       } else if (type == "2") {
@@ -52,21 +78,29 @@ function saveData() {
     }
   }
 }
+
+function showLogout() {
+  user = {};
+  showHeader();
+  showHome();
+}
 function home() {
+  showHeader();
   let str = `
+  <div class="w-100 bg-light p-5 m-5 rounded text-center">
       <h3>Welcome ${user.name}</h3>
       <button onclick='showLogin()'>Logout</button>
-      <p><select id="type" onchange='showUser()'>
+      <p><select id="type" class="form-control" onchange='showUser()'>
          <option value=0>--Select--</option>
          <option value=1>Deposit</option>
          <option value=2>Withdraw</option>
          <option value=3>Transfer</option>
          </select></p>
-         <p><select style="display:none" id="selUser"></select></p>
-         <p><input type="number" id="amount" placeholder="Enter Amount"></p>
-         <button onclick='saveData()'>Submit</button>
+         <p><select style="display:none" id="selUser" class="form-control"></select></p>
+         <p><input type="number" id="amount" class="form-control" placeholder="Enter Amount"></p>
+         <button onclick='saveData()' class="form-control btn btn-success">Submit</button>
          <p><b>Current Balance: <span id='spBalance'>${user.balance}</span></b></p>
-
+    </div>
       `;
   root.innerHTML = str;
 }
@@ -93,7 +127,7 @@ function chkUser() {
       // useremail = email;
       // username = users[i].name;
       // currBalance = users[i].balance;
-      user = users[i]
+      user = users[i];
       home();
       break;
     } else {
@@ -103,28 +137,51 @@ function chkUser() {
 }
 function showForm() {
   let str = `
-  <h2>Registration Form</h2>
-  <p><input type="text" id="name" placeholder="Name"></p>
-  <p><input type="text" id="email" placeholder="Email"></p>
-  <p><input type="password" id="password" placeholder="Password"></p>
-  <p><input type="date" id="dob"></p>
-  <p><button onclick='addUser()'>Submit</button></p>
-  <p>Already a member?<button onclick='showLogin()'>Login Here</button></p>
+  <div class="w-100 bg-secondary p-5 m-5 rounded text-center">
+  <h2 class="text-warning">Registration Form</h2>
+  <p><input type="text" class="form-control" id="name" placeholder="Name"></p>
+  <p><input type="text" class="form-control" id="email" placeholder="Email"></p>
+  <p><input type="password" class="form-control" id="password" placeholder="Password"></p>
+  <p><input type="date" class="form-control" id="dob"></p>
+  <p><button onclick='addUser()' class="form-control btn btn-warning">Submit</button></p>
+  <p>Already a member?<button onclick='showLogin()' class="form-control btn btn-warning">Login Here</button></p>
+  </div>
   `;
   root.innerHTML = str;
 }
 function showLogin() {
   let str = `
-  <div>
-      <h2>Login Form</h2>
+  <div class="w-100 bg-light m-3 p-5 rounded text-center">
+      <h2 class="text-dark">Login Form</h2>
       <div id='msg'></div>
-      <p><input id="email" type="text"></p>
-      <p><input id="password" type="password"></p>
-      <button onclick='chkUser()'>Log In</button>
-      <p><button onclick='showForm()'>Create Account</button></p>
+      <p><input id="email" class='form-control' placeholder='Email address' type="text"></p>
+      <p><input id="password" class='form-control' placeholder='Password' type="password"></p>
+      <button onclick='chkUser()' class="btn btn-primary w-100">Log In</button>
+      <p><button onclick='showForm()' class="btn btn-success mt-3">Create Account</button></p>
   </div>
   `;
   root.innerHTML = str;
 }
 
-showLogin();
+function showHome() {
+  showHeader();
+  let str = `
+<div class="card text-center">
+  <div class="card-header">
+    Banking...!!
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Know your details.....!!!!</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <button onclick='showForm()' class="btn btn-success">Create Account</button>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
+
+`;
+  root.innerHTML = str;
+}
+
+showHome();
